8e70342399c29db68267e684650b585f99f1b969
derickcux2023 <derickcux2023@LAB4-20>
2024-02-12 09:31:31 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace archivosTextoProyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLeer = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonLeerDirecto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(93, 146);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuardar.TabIndex = 0;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonLeer
+            // 
+            this.buttonLeer.Location = new System.Drawing.Point(490, 177);
+            this.buttonLeer.Name = "buttonLeer";
+            this.buttonLeer.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeer.TabIndex = 2;
+            this.buttonLeer.Text = "Leer";
+            this.buttonLeer.UseVisualStyleBackColor = true;
+            this.buttonLeer.Click += new System.EventHandler(this.buttonLeer_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(490, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(273, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonLeerDirecto
+            // 
+            this.buttonLeerDirecto.Location = new System.Drawing.Point(490, 232);
+            this.buttonLeerDirecto.Name = "buttonLeerDirecto";
+            this.buttonLeerDirecto.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeerDirecto.TabIndex = 4;
+            this.buttonLeerDirecto.Text = "Leer directo";
+            this.buttonLeerDirecto.UseVisualStyleBackColor = true;
+            this.buttonLeerDirecto.Click += new System.EventHandler(this.buttonLeerDirecto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLeerDirecto);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonLeer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonGuardar);
+            this.Name = "Form1";
+            this.Text = "Archivo de texto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonLeer;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonLeerDirecto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace archivosTextoProyecto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardar(string fileName, string texto)
+        {
+            //Abrir el archivo: Write sobreescribe el archivo, Append agrega los datos al final del archivo
+            FileStream stream = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+            //Crear un objeto para escribir el archivo
+            StreamWriter writer = new StreamWriter(stream);
+            //Usar el objeto para escribir al archivo, WriteLine, escribe linea por linea
+            //Write escribe todo en la misma linea. En este ejemplo se hará un dato por cada línea
+            writer.WriteLine(texto);
+            //Cerrar el archivo
+            writer.Close();
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            Guardar(@"C:\Users\derickcux2023\Desktop\PruebaGuardado.txt", textBox1.Text);
+            MessageBox.Show("Texto Guardado :)");
+        }
+
+        private void buttonLeer_Click(object sender, EventArgs e)
+        {
+            //Ademas de utilizar el control openFileDialog, es necesario declarar un objeto de este tipo
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //Directorio en donde se va a iniciar la busqueda
+            openFileDialog1.InitialDirectory = @"C:\Users\derickcux2023\Desktop";
+            //Tipos de archivos que se van a buscar, en este caso archivos de texto con extensión .txt
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            //Muestra la ventana para abrir el archivo y verifica que si se pueda abrir
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                //Guardamos en una variable el nombre del archivo que abrimos
+                string fileName = openFileDialog1.FileName;
+
+                //Abrimos el archivo, en este caso lo abrimos para lectura
+                FileStream flujo = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                StreamReader lector = new StreamReader(flujo);
+
+                //Un ciclo para leer el archivo hasta el final del archivo
+                //Lo leído se va guardando en un control richTextBox
+                while (lector.Peek() > -1)
+                //Esta linea envía el texto leído a un control richTextBox, se puede cambiar para que
+                //lo muestre en otro control por ejemplo un combobox
+                {
+                    string textoLeido = lector.ReadLine();
+                    richTextBox1.AppendText(textoLeido);
+                }
+                //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+                lector.Close();
+            }
+        }
+
+        private void buttonLeerDirecto_Click(object sender, EventArgs e)
+        {
+            string fileName = @"C:\Users\derickcux2023\Desktop\PruebaGuardado.txt";
+
+            //Abrimos el archivo, en este caso lo abrimos para lectura
+            FileStream flujo = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader lector = new StreamReader(flujo);
+
+            //Un ciclo para leer el archivo hasta el final del archivo
+            //Lo leído se va guardando en un control richTextBox
+            while (lector.Peek() > -1)
+            //Esta linea envía el texto leído a un control richTextBox, se puede cambiar para que
+            //lo muestre en otro control por ejemplo un combobox
+            {
+                string textoLeido = lector.ReadLine();
+                richTextBox1.AppendText(textoLeido);
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            lector.Close();
+        }
+    }
+}
